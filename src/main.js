958ba8b6d1f3a134b1b918bdc21e5ef104e714ea
Nick Matantsev <nick.m@myplanetdigital.com>
2014-03-01 23:52:51 -0500

    Added bike definition

--- src/main.js
+++ src/main.js
@@ -32,8 +32,42 @@ require([ 'stats' ], function (Stats) {
 
     var world;
 
+    function createBike(x, y) {
+        var fixDef = new b2FixtureDef();
+        fixDef.density = 1.0;
+        fixDef.friction = 0.5;
+        fixDef.restitution = 0.2;
+        fixDef.shape = new b2CircleShape(0.5);
+
+        var bodyDef = new b2BodyDef();
+        bodyDef.type = b2Body.b2_dynamicBody;
+        bodyDef.position.x = x - 0.75;
+        bodyDef.position.y = y;
+
+        var w1 = world.CreateBody(bodyDef);
+        w1.CreateFixture(fixDef);
+
+        bodyDef.position.x = x + 0.75;
+        var w2 = world.CreateBody(bodyDef);
+        w2.CreateFixture(fixDef);
+
+        bodyDef.position.x = x;
+        fixDef.shape = new b2CircleShape(0.1);
+        var main = world.CreateBody(bodyDef);
+        main.CreateFixture(fixDef);
+
+        var rjd = new b2RevoluteJointDef();
+        rjd.Initialize(w1, main, new b2Vec2(x - 0.75, y));
+        world.CreateJoint(rjd);
+
+        rjd.Initialize(w2, main, new b2Vec2(x + 0.75, y));
+        world.CreateJoint(rjd);
+    }
+
     function init() {
        world = new b2World(new b2Vec2(0, 10), true);
+
+       createBike(2, 5);
 
        var SCALE = 30;
 
@@ -59,7 +93,7 @@ require([ 'stats' ], function (Stats) {
 
        //create some objects
        bodyDef.type = b2Body.b2_dynamicBody;
-       for(var i = 0; i < 150; ++i) {
+       for(var i = 0; i < 15; ++i) {
           if(Math.random() > 0.5) {
              fixDef.shape = new b2PolygonShape;
              fixDef.shape.SetAsBox(
